929aa74acc3b83042b79efd318c4d5afc32c5407
notalentwhatsoever <74pp4khvbt@privaterelay.appleid.com>
2026-05-13 14:51:37 +0900

    chore: 채팅 추천 UI 헬퍼와 시드 데이터를 정리한다

---
+++ app/components/chat/chat-message-seed.ts
@@ -0,0 +1,67 @@
+import type { ChatMessage } from "./types";
+
+export const initialChatMessages: ChatMessage[] = [
+    {
+        id: "young-geol-1",
+        friendId: "young-geol",
+        sender: "friend",
+        text: "지금 어디야?",
+        time: "오전 10:12",
+    },
+    {
+        id: "young-geol-2",
+        friendId: "young-geol",
+        sender: "me",
+        text: "지금 학교에 있어",
+        time: "오전 10:14",
+    },
+    {
+        id: "young-geol-3",
+        friendId: "young-geol",
+        sender: "friend",
+        text: "난 강남역 근처야",
+        time: "오전 10:17",
+    },
+    {
+        id: "young-geol-4",
+        friendId: "young-geol",
+        sender: "me",
+        text: "그럼 우리 중간쯤에서 만나자",
+        time: "오전 10:18",
+    },
+    {
+        id: "young-geol-5",
+        friendId: "young-geol",
+        sender: "friend",
+        text: "좋아 지도에서 중간 지점 확인해보자",
+        time: "오전 10:20",
+    },
+    {
+        id: "young-jun-1",
+        friendId: "young-jun",
+        sender: "friend",
+        text: "오늘 저녁에 시간 괜찮아?",
+        time: "오후 01:08",
+    },
+    {
+        id: "young-jun-2",
+        friendId: "young-jun",
+        sender: "me",
+        text: "좋아. 위치 공유하면 중간 지점부터 보자.",
+        time: "오후 01:10",
+    },
+    {
+        id: "ji-min-1",
+        friendId: "ji-min",
+        sender: "friend",
+        text: "곧 출발할게. 카페 쪽으로 갈 수 있어.",
+        time: "오후 03:25",
+    },
+    {
+        id: "min-seo-1",
+        friendId: "min-seo",
+        sender: "friend",
+        text: "카페 앞에 도착했어. 지도에서 위치 볼래?",
+        time: "오후 05:42",
+    },
+];
